4ef9168c9b0335e8e4087f71074f141c710c6536
kanishk <kanishk.coe>
2021-02-07 09:10:51 +0530

    POJOs created. Closes #2

---
+++ prometheusDemo/src/main/java/com/kanishk/prometheusDemo/model/Employee.java
@@ -0,0 +1,51 @@
+package com.kanishk.prometheusDemo.model;
+
+public class Employee {
+
+    String firstName;
+    String lastName;
+    Float salary;
+    Address address;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", address=" + address.toString() +
+                '}';
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+}
